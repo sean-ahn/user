2b6a0659f8798a160e3c6181903bccbb093368b0
sean-ahn <sean9509@gmail.com>
2021-10-21 16:11:30 +0900

    Add cmd

---
+++ backend/cmd/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	"github.com/sean-ahn/user/backend/config"
+	"github.com/sean-ahn/user/backend/server"
+)
+
+func main() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	if err := run(); err != nil {
+		logrus.Panic(err)
+	}
+}
+
+func run() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	setting := config.NewSetting()
+
+	cfg := config.New(setting)
+
+	grpcServer, err := server.NewGRPCServer(cfg)
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	httpServer, err := server.NewHTTPServer(ctx, cfg)
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	go func() {
+		lis, err := net.Listen("tcp", ":"+strconv.Itoa(setting.GRPCServerPort))
+		if err != nil {
+			logrus.Panic(err)
+		}
+
+		logrus.WithField("port", setting.GRPCServerPort).Info("starting gRPC server")
+		if err := grpcServer.Serve(lis); err != nil && err != grpc.ErrServerStopped {
+			logrus.Panic(err)
+		}
+	}()
+
+	go func() {
+		logrus.WithField("port", setting.HTTPServerPort).Info("starting HTTP server")
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logrus.Panic(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(quit)
+
+	<-quit
+
+	time.Sleep(time.Duration(setting.GracefulShutdownTimeoutMs) * time.Millisecond)
+
+	logrus.Info("stopping HTTP server")
+	if err := httpServer.Shutdown(ctx); err != nil {
+		logrus.Errorf("shutdown http server: %w", err)
+	}
+
+	logrus.Info("stopping gRPC server")
+	grpcServer.GracefulStop()
+
+	return nil
+}
