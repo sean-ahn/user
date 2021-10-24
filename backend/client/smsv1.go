4fcb0a483dac4697d3b836af4febe42c2707ad63
sean-ahn <sean9509@gmail.com>
2021-10-25 02:04:43 +0900

    Add smsv1 service client

---
+++ backend/client/smsv1.go
@@ -0,0 +1,38 @@
+package client
+
+//go:generate mockgen -package client -destination ./smsv1_client_mock.go -mock_names SmsServiceClient=MockSmsServiceClient github.com/sean-ahn/user/proto/gen/go/sms/v1 SmsServiceClient
+
+import (
+	"sync"
+
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	smsv1 "github.com/sean-ahn/user/proto/gen/go/sms/v1"
+)
+
+const smsServiceConfig = `{"loadBalancingPolicy":"round_robin"}`
+
+var (
+	smsOnce sync.Once
+	smsCli  smsv1.SmsServiceClient
+
+	_ smsv1.SmsServiceClient = (*MockSmsServiceClient)(nil)
+)
+
+func GetSmsServiceClient(serviceHost string) smsv1.SmsServiceClient {
+	smsOnce.Do(func() {
+		conn, err := grpc.Dial(
+			serviceHost,
+			grpc.WithInsecure(),
+			grpc.WithDefaultServiceConfig(smsServiceConfig),
+		)
+		if err != nil {
+			logrus.Panic(err)
+		}
+
+		smsCli = smsv1.NewSmsServiceClient(conn)
+	})
+
+	return smsCli
+}
