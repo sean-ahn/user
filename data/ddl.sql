e2447315db51824cc9ff80e44311923da51d0abc
sean-ahn <sean9509@gmail.com>
2021-10-25 03:10:00 +0900

    Update

--- data/ddl.sql
+++ data/ddl.sql
@@ -4,12 +4,12 @@ USE `user`;
 CREATE TABLE `user`
 (
     `user_id`           int          NOT NULL AUTO_INCREMENT COMMENT '유저 아이디',
-    `name`              varchar(10)  NOT NULL COMMENT '이름',     -- 실명 검증 안 된 이름
+    `name`              varchar(10)  NOT NULL COMMENT '이름',     -- NOTE: may not be real name
     `email`             varchar(128) NOT NULL COMMENT '이메일',
     `is_email_verified` tinyint(1)   NOT NULL DEFAULT '0' COMMENT '이메일 검증 여부',
     `phone_number`      varchar(15)  NOT NULL COMMENT '핸드폰 번호', -- format: E.164 (e.g. +821012345678)
     `nickname`          varchar(15)  NOT NULL COMMENT '닉네임',
-    `password_hash`     varchar(86)  NOT NULL COMMENT '비밀번호',   -- base64 encoded scrypt output w/ salt: 16 bytes, N: 32768, r: 8, p: 1, len: 32
+    `password_hash`     varchar(86)  NOT NULL COMMENT '비밀번호',   -- format: base64 encoded 32 bytes scrypt output w/ salt: 16 bytes, N: 32768, r: 8, p: 1, keyLen: 32
     `created_at`        timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`        timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`user_id`),
@@ -25,8 +25,8 @@ CREATE TABLE `user`
 CREATE TABLE `jwt_audience_secret`
 (
     `jwt_audience_secret_id` int         NOT NULL AUTO_INCREMENT COMMENT 'JWT audience secret 아이디',
-    `audience`               varchar(10) NOT NULL COMMENT 'JWT audience', -- user.user_id
-    `secret`                 varchar(44) NOT NULL COMMENT 'secret',       -- base64 encoded 256 bits
+    `audience`               varchar(10) NOT NULL COMMENT 'JWT audience',
+    `secret`                 varchar(44) NOT NULL COMMENT 'secret', -- format: base64 encoded 256 bits
     `created_at`             timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`             timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`jwt_audience_secret_id`),
@@ -41,8 +41,8 @@ CREATE TABLE `jwt_audience_secret`
 CREATE TABLE `jwt_denylist`
 (
     `jwt_denylist_id` int         NOT NULL AUTO_INCREMENT COMMENT 'JWT denylist 아이디',
-    `user_id`         int         NOT NULL COMMENT '유저 아이디',
-    `jti`             varchar(36) NOT NULL COMMENT 'jti(JWT ID) claim', -- uuid v4
+    `user_id`         int         NOT NULL COMMENT '유저 아이디',            -- user.user_id
+    `jti`             varchar(36) NOT NULL COMMENT 'jti(JWT ID) claim', -- format: uuid v4
     `created_at`      timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`      timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`jwt_denylist_id`),
@@ -52,6 +52,7 @@ CREATE TABLE `jwt_denylist`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci COMMENT ='JWT denylist';
+
 
 CREATE TABLE `sms_otp_verification`
 (
